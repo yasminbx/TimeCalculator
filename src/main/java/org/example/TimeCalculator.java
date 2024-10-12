2d8b2d0e24cefbd856fc9f17f2b894a0bbcd05e8
Yasmin Belhout <yasmin.belhout@metropolia.fi>
2024-10-12 20:02:33 +0300

    first

---
+++ src/main/java/org/example/TimeCalculator.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.lang.IllegalArgumentException;
+
+public class TimeCalculator {
+    public double calculateTime(double distance, double speed) {
+        if (speed <= 0) {
+            throw new IllegalArgumentException("Speed must be greater than zero.");
+        }
+        return distance / speed;
+    }
+}
